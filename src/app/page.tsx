e8e0cb5fcb53152ec90086da2b928df8d10fb7e8
Ahmad Aizuddin <aizuddinahmd0408@gmail.com>
2025-12-22 20:47:57 +0800

    deleted features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import FeaturedProjects from "@/components/FeaturedProjects";
 import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
 import Services from "@/components/Services";
-import Features from "@/components/Features";
+// import Features from "@/components/Features";
 
 export default function Home() {
   return (
     <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
       <Hero />
       <Services />
-      <Features />
+      {/* <Features /> */}
       <FeaturedProjects />
       <Skills />
       <Experience />
