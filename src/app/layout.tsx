255f90886b28777a24d573c63436b6929ee8c3ed
Ahmad Aizuddin <aizuddinahmd0408@gmail.com>
2025-12-24 23:45:08 +0800

    update analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Header />
         <main className="">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
